1e8fea3e80387ba8e6012e4b6e8dee5bd56ea6f2
Daniel Tjandra <danieltjandra@gmail.com>
2014-06-08 18:19:26 -0400

    A1 V1.9

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 354 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#354 SMP PREEMPT Fri Jan 17 00:56:24 EST 2014"
+#define LINUX_COMPILE_BY "Daniel"
+#define LINUX_COMPILE_HOST "Daniel3"
+#define LINUX_COMPILER "gcc version 4.5.1 (Sourcery G++ Lite 2010.09-51) "
